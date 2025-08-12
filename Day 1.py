45721299ec24b74bd1838baecf985a95ed9c92f2
June-Ma <june.ma0810@gmail.com>
2025-08-12 09:39:26 +0100

    initial commit: Day 1

---
+++ Day 1.py
@@ -0,0 +1,13 @@
+print("Hellow world")
+my_list = [1,2,2,3,4,5]
+my_list.append(8)
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+
+my_info={"name":"June","age":15,"city":"Canterbury"}
+print(my_info)
+
+s="horse eat hay"
+print(s.capitalize())
+print(s.upper())
